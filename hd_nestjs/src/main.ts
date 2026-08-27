4dfff1d0ff873a47e183836c31a1403bb08c4e87
NidaArshad1908 <nidaarshad113@gmail.com>
2026-08-27 08:14:40 +0500

    dev- Cors add

--- hd_nestjs/src/main.ts
+++ hd_nestjs/src/main.ts
@@ -4,7 +4,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: process.env.FRONTEND_URL ?? 'http://localhost:3000' });
+  app.enableCors({
+    origin: [
+      process.env.FRONTEND_URL ?? 'http://localhost:3000',
+      'https://hide-design.vercel.app',
+      'https://hidesdesign.com'
+    ],
+     credentials: true,
+  });
 
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
